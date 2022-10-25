83f94af19ea5259694c1314fb9b1d0f5be6e954b
redpumpkin21 <christopher.johnson2108@gmail.com>
2022-10-25 18:27:24 -0400

    git fix?

---
+++ src/Components.js/display.js
@@ -0,0 +1,7 @@
+const Display = () => {
+    return(
+        <h1>hi</h1>
+    )
+}
+
+export default Display
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Route>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Route>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
