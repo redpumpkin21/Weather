ce4ed8090cdc054bb8ca13a6695afe8196cb5589
redpumpkin21 <christopher.johnson2108@gmail.com>
2022-10-26 16:03:06 -0400

    async done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react'
 function App() {
+  const [weather, setWeather] = useState({})
+  let apiKey = '11786cf53362e428a0695f06c5ece387'
+  const getWeather = async(zipCode) =>{
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${apiKey}`)
+    const data = await response.json()
+    setWeather(data)
+  }
+
+  useEffect (() => {getWeather()}, [])
   return (
     <div className="App">
       <header className="App-header">
