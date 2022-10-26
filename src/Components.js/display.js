179dc4b3e793b63a8616e62328516b91b1104709
redpumpkin21 <christopher.johnson2108@gmail.com>
2022-10-26 16:38:38 -0400

    more simple

--- src/Components.js/display.js
+++ src/Components.js/display.js
@@ -1,43 +1,16 @@
 const Display = ({weather}) => {
-    let currentTemp = weather.main?.temp;
-        if(currentTemp> 90){
-            currentTemp =<h1 style = {{ color:'red'}}>{currentTemp}</h1>
-        }else if( currentTemp < 40){
-            currentTemp =<h1 style ={{ color:'blue'}}>{currentTemp}</h1>
-    }else{
-        currentTemp = <h1>{currentTemp}</h1>
-    }
-      
-
-        
     
-     const loaded = () => {
-    return(
-        <>
-        <div>{weather.name}</div>
-        <h1>{currentTemp}</h1>
-        {/* <h1 > {weather.main?.temp}</h1>  */}
-        
-        <h3>{weather.weather[0].description}</h3>
-        
-        <div className ='min-max' >
-            <div className = 'temps' id = 'min'>
-                <small>min</small>
-                <h2>
-                    {weather.main?.temp_min}
-                </h2>
-             </div>
-             <div className = 'temps' id= 'max'>
-                    <small>max</small>
-                    <h2>
-                    {weather.main?.temp_max}
-                    </h2>
-             </div>
-        </div>
-        </>
-        
-    )}
-    console.log(loaded)
+    const loaded = () => {
+        return(
+
+            <div>
+                <p>{weather.name}</p>
+                <h1 className="temp">{weather.temp}</h1>
+                <p>{weather.weather[0].description}</p>
+    
+            </div>
+               
+        )}    
     const loading = () => {
         return <h1>Put in your Zip code to learn more</h1>
     }
